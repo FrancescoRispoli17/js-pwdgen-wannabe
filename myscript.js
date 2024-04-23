a2d2d4301eaef9a6b34ec31eeb3ac39ac215f967
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-04-23 16:57:55 +0200

    [UPD] input added in the page and in console

--- myscript.js
+++ myscript.js
@@ -1,3 +1,8 @@
 const Name= prompt('inserisci il tuo nome');
 const Last_Name= prompt('inserisci il tuo cognome');
 const Color= prompt('inserisci il tuo colore preferito');
+const n= Math.floor(Math.random()*100);
+let p= document.getElementById('testo').innerHTML;
+p=Name + Last_Name + Color + n;
+console.log(p);
+document.getElementById('testo').innerHTML=p;
